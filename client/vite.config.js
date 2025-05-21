ff0ff4dd18ebcbb83af91249e3874de2de0e0cea
Aliyu Yunus <aliyuyunus483@gmail.com>
2025-05-21 09:15:23 +0300

    front end deployement changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   // optimizeDeps: {
   //   include: ["@chapa_et/inline.js"], // Force Vite to include the package
   // },
